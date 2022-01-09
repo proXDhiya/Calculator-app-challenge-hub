6d0d8c86dac5dc5951649838d70baaf8c3b42899
proXDhiya <prox.dhiya@gmail.com>
2022-01-09 22:51:37 +0100

    start with calculator logic

---
+++ js/app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// add calculator button
+const btnNum1 = document.querySelector('#num_1');
+const btnNum2 = document.querySelector('#num_2');
+const btnNum3 = document.querySelector('#num_3');
+const btnNum4 = document.querySelector('#num_4');
+const btnNum5 = document.querySelector('#num_5');
+const btnNum6 = document.querySelector('#num_6');
+const btnNum7 = document.querySelector('#num_7');
+const btnNum8 = document.querySelector('#num_8');
+const btnNum9 = document.querySelector('#num_9');
+const btnNum0 = document.querySelector('#num_0');
+const btnAdd = document.querySelector('#add');
+const btnSub = document.querySelector('#sub');
+const btnMul = document.querySelector('#mul');
+const btnDiv = document.querySelector('#div');
+const btnDel = document.querySelector('#del');
+const btnEql = document.querySelector('#result');
+const btnReset = document.querySelector('#reset');
+const btnDot = document.querySelector('#point');
+
+// add screen result
+const screen = document.querySelector('.screen > p');
